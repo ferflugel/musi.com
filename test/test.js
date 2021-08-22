ff7edd6f5feb21c9e91b4407dc975319f8a4d78a
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-08-22 19:17:41 -0300

    ...

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-var counts = ['musica1','musica2','musica3','musica4'];
+var counts = ['musica1','musica2','musica3','musica4', 'musica5','musica6','musica7','musica8', 'musica9','musica10'];
+
 
 var container = document.getElementById('container');
 var originalForm = document.getElementById('rateForm');
@@ -13,8 +14,8 @@ counts.forEach(function(count) {
     text.innerHTML = "<p>" + count + "</p>";
     node.querySelectorAll('input').forEach(function(elem, i) {
         elem.setAttribute("name", count);
-        elem.setAttribute("id", count + i);
-        elem.nextSibling.nextSibling.setAttribute("for", count + i);
+        elem.setAttribute("id", count + "_" + i);
+        elem.nextSibling.nextSibling.setAttribute("for", count + "_" + i);
         var s = "updateVal('" + count + "')";
         elem.setAttribute("onclick", s);
     });
@@ -28,4 +29,17 @@ counts.forEach(function(count) {
 function updateVal(i) {
     const val = document.querySelector("input[name='"+ i + "']:checked").value;
     console.log("Input - " + i + " : " + val);
+    document.querySelector("input[name='"+ i + "']:checked").parentElement.setAttribute("name", val);
+    getAverage();
+}
+
+function getAverage() {
+    var musics = [].slice.call(document.getElementsByClassName("colorBar"));
+    var total = 0;
+    musics.forEach(function(elem) {
+      total += parseInt(elem.getAttribute("name"));
+    })
+    var average = (total - 5) / (musics.length - 1);
+    var paragraph = document.getElementById('OverallScore');
+    paragraph.innerHTML = "Album Average Score: " + average;
 }
