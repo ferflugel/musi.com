78759fe8095f7d6d0feaf13093f5dbc6a7c8be97
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-09-01 11:26:37 -0300

    Hover gradient change effect

--- V2_Chung/script.js
+++ V2_Chung/script.js
@@ -1,13 +1,42 @@
+// Sets the start time of the video
 document.getElementsByClassName('musicPage')[0].children[0].addEventListener('loadedmetadata', function() {
   this.currentTime = 57;
 }, false);
 
-let video = document.getElementById('video');
-video.muted = !video.muted;
+function hoveringEffect(id) {
+  let temp = [ 0,   0,   0,   0,   0];
+  for(let i = 0; i <= parseInt(id); i++) {
+    temp[i] = 1
+  }
+
+  for(let i = 0; i < temp.length; i++) {
+    let elem = document.getElementById(String(i));
+    if(temp[i] == 1) {
+      // Changes style if element is clicked (or below the last clicked one)
+      elem.style.backgroundColor = "rgba(0,204,125,"+(i+3)*(0.8/temp.length)+")";  //The (i+3) controls the shift in transparency, while the n/arr.length controls the overall transparency
+    } else {
+      elem.style.backgroundColor = "rgba(200,200,200,0.3)";
+    }
+  }
+}
 
 
+function resetCSS() {
+  for(let i = 0; i < arr.length; i++) {
+    let elem = document.getElementById(String(i));
+    if(arr[i] == 1) {
+      // Changes style if element is clicked (or below the last clicked one)
+      elem.style.backgroundColor = "rgba(0,204,125,"+(i+3)*(0.8/arr.length)+")";  //The (i+3) controls the shift in transparency, while the n/arr.length controls the overall transparency
+    } else {
+      elem.style.backgroundColor = "rgba(200,200,200,0.3)";
+    }
+  }
+}
+
+// Updates the colors of rating buttons whenever one of them is clicked
+let arr = [ 0,   0,   0,   0,   0];
 function updateVal(id) {
-  let arr = [ 0,   0,   0,   0,   0];
+  arr = [0, 0, 0, 0, 0];
   for(let i = 0; i <= parseInt(id); i++) {
     arr[i] = 1
   }
@@ -22,5 +51,4 @@ function updateVal(id) {
     }
   }
   console.log(arr);
-
-}
+};
