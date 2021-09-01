83bd83eb614b0b51f325bba4f16abe17b096feb7
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-09-01 10:48:55 -0300

    Gradient thingy working...

--- V2_Chung/script.js
+++ V2_Chung/script.js
@@ -2,8 +2,24 @@ document.getElementsByClassName('musicPage')[0].children[0].addEventListener('lo
   this.currentTime = 57;
 }, false);
 
-function updateVal(i) {
-    const val = document.querySelector("input:checked").value;
-    console.log("Input - " + i + " : " + val);
-    document.querySelector("input:checked").parentElement.setAttribute("name", val);
+
+
+function updateVal(id) {
+  let arr = [ 0,   0,   0,   0,   0];
+  for(let i = 0; i <= parseInt(id); i++) {
+    arr[i] = 1
+  }
+
+  for(let i = 0; i < arr.length; i++) {
+    let elem = document.getElementById(String(i));
+    if(arr[i] == 1) {
+      // Changes style if element is clicked (or below the last clicked one)
+      elem.style.backgroundColor = "rgba(0,204,125,"+(i+3)*(0.8/arr.length)+")";  //The (i+3) controls the shift in transparency, while the n/arr.length controls the overall transparency
+    } else {
+      elem.style.backgroundColor = "rgba(200,200,200,0.3)";
+    }
+  }
+  console.log(arr);
+
+
 }
