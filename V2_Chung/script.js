ec6b4d7e05e781921b28441d10facdf327441ae7
Jhony <34777275+Jhouny@users.noreply.github.com>
2021-09-07 17:24:45 -0300

    Working music change buttons

--- V2_Chung/script.js
+++ V2_Chung/script.js
@@ -1,11 +1,25 @@
+//Hides other musics from the screen
 const p = document.getElementsByClassName('musicPage');
+const musics = []
+var activeMusic = "Starboy";
 Array.prototype.forEach.call(p, elem => {
+  musics.push(elem.getAttribute('id'));
   if(elem.getAttribute('name') != "Active") {
     elem.style.display = 'none';
   }
 });
 
-
+//Updates the active tab
+function updateActive() {
+  Array.prototype.forEach.call(p, elem => {
+    if(elem.getAttribute('name') != "Active") {
+      elem.style.display = 'none';
+    } else if ( elem.getAttribute('name') == "Active" ) {
+      elem.style.display = 'block';
+      activeMusic = elem.getAttribute('id');
+    }
+  });
+}
 
 // Sets the start time of the video
 document.getElementsByClassName('musicPage')[0].children[0].addEventListener('loadedmetadata', function() {
@@ -15,12 +29,12 @@ document.getElementsByClassName('musicPage')[0].children[0].addEventListener('lo
 // Hover effect on rating buttons
 function hoveringEffect(id) {
   let temp = [ 0,   0,   0,   0,   0];
-  for(let i = 0; i <= parseInt(id); i++) {
+  for(let i = 0; i <= parseInt(id.split(activeMusic)[1]); i++) {
     temp[i] = 1
   }
 
   for(let i = 0; i < temp.length; i++) {
-    let elem = document.getElementById(String(i));
+    let elem = document.getElementById(activeMusic + String(i));
     if(temp[i] == 1) {
       // Changes style if element is clicked (or below the last clicked one)
       elem.style.backgroundColor = "rgba(0,204,125,"+(i+3)*(0.8/temp.length)+")";  //The (i+3) controls the shift in transparency, while the n/arr.length controls the overall transparency
@@ -30,10 +44,10 @@ function hoveringEffect(id) {
   }
 }
 
-
+//Resets score-bar css state
 function resetCSS() {
   for(let i = 0; i < arr.length; i++) {
-    let elem = document.getElementById(String(i));
+    let elem = document.getElementById(activeMusic + String(i));
     if(arr[i] == 1) {
       // Changes style if element is clicked (or below the last clicked one)
       elem.style.backgroundColor = "rgba(0,204,125,"+(i+3)*(0.8/arr.length)+")";  //The (i+3) controls the shift in transparency, while the n/arr.length controls the overall transparency
@@ -47,12 +61,12 @@ function resetCSS() {
 let arr = [ 0,   0,   0,   0,   0];
 function updateVal(id) {
   arr = [0, 0, 0, 0, 0];
-  for(let i = 0; i <= parseInt(id); i++) {
+  for(let i = 0; i <= parseInt(id.split(activeMusic)[1]); i++) {
     arr[i] = 1
   }
 
   for(let i = 0; i < arr.length; i++) {
-    let elem = document.getElementById(String(i));
+    let elem = document.getElementById(activeMusic + String(i));
     if(arr[i] == 1) {
       // Changes style if element is clicked (or below the last clicked one)
       elem.style.backgroundColor = "rgba(0,204,125,"+(i+3)*(0.8/arr.length)+")";  //The (i+3) controls the shift in transparency, while the n/arr.length controls the overall transparency
@@ -62,3 +76,50 @@ function updateVal(id) {
   }
   console.log(arr);
 };
+
+//Handles negative indexes on arrays
+const proxy = new Proxy(musics, {
+    get(target, prop) {
+        if (!isNaN(prop)) {
+            prop = parseInt(prop, 10);
+            if (prop < 0) {
+                prop += target.length;
+            }
+        }
+        return target[prop%target.length];
+    }
+});
+
+//Changes current active music
+
+// function PrevMusic() {
+//   arr = [ 0,   0,   0,   0,   0];
+//   for (var i = 0; i < musics.length; i++) {
+//     elem = document.getElementById(proxy[i]);
+//     if (elem.getAttribute('name') == "Active") {
+//       elem.setAttribute('name', " ");
+//       activate = i - 1;
+//     } else if (elem == document.getElementById(proxy[activate])) {
+//       elem.setAttribute('name', "Active");
+//     }
+//   }
+//   updateActive();
+// }
+
+function PrevMusic() {
+  arr = [ 0,   0,   0,   0,   0];
+  elem = document.getElementById(activeMusic);
+  elem.setAttribute('name', " ");
+  var index = musics.indexOf(elem.getAttribute('id'))-1;
+  document.getElementById(proxy[index]).setAttribute('name', "Active");
+  updateActive();
+}
+
+function NextMusic() {
+  arr = [ 0,   0,   0,   0,   0];
+  elem = document.getElementById(activeMusic);
+  elem.setAttribute('name', " ");
+  var index = musics.indexOf(elem.getAttribute('id'))+1;
+  document.getElementById(proxy[index]).setAttribute('name', "Active");
+  updateActive();
+}
