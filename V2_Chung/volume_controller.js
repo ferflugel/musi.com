9e037f6d2a39c2b0395a853d87bf4c37b2d5d97a
Jhouny <jhonyaraujo9121@gmail.com>
2021-10-14 15:45:14 -0300

    Initial animation to _show_ where the volume slider is...

--- V2_Chung/volume_controller.js
+++ V2_Chung/volume_controller.js
@@ -1,4 +1,5 @@
-let  volSlider = document.getElementById('volRange');
+let volSlider = document.getElementById('volRange');
+let volAnim = document.getElementById('volumeSlider').getElementsByTagName('i')[0];
 let audioElements = document.getElementsByTagName('audio');
 
 function setVolume() {
@@ -6,3 +7,20 @@ function setVolume() {
     aud.volume = ((parseInt(volSlider.value)-1)/100);
   })
 }
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    volSlider.style.visibility = 'visible';
+    volSlider.style.opacity = '0.8';
+    volSlider.style.marginTop = '64px';
+
+    volAnim.style.visibility = 'visible';
+    volAnim.style.opacity = '1';
+    volAnim.style.marginTop = '80px';
+  }, 200);
+
+  setTimeout(function() {
+    volSlider.attributeStyleMap.clear();
+    volAnim.attributeStyleMap.clear();
+  }, 2500);
+})
